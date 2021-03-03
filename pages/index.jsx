39540a23e7aec79b698929f82b9684320360aeeb
zohaib <zohaibansari100@gmail.com>
2021-03-03 21:19:36 +0530

    new homepage

--- pages/index.jsx
+++ pages/index.jsx
@@ -3,35 +3,31 @@ import Head from 'next/head'
 import Post from '../components/post'
 import Link from 'next/link'
 
-const client = require('contentful').createClient({
-  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
-  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
-})
-
-export async function getStaticProps(){
-  let data = await client.getEntries({
-    content_type: 'post'
-  })
-
-  return {
-    props: {
-      posts: data.items
-    },
-    revalidate: 1
-  }
-}
-
 export default function Home(posts){
   console.log(posts.posts);
   return (
+    <>
+    <div class="header">
+      <h1 class="titleName">Zohaib Ansari.</h1>
+      <p><h3 class="subtext">Android Developer</h3></p>
+    </div>
+    <p class="itemList">
       <ul>
-      {posts.posts.map(article => (
-        <li key={article.sys.id}>
-          <Link href={'/posts/' + article.fields.slug}>
-          <a>{article.fields.title}</a>
+        <li>Portfolio</li>
+        <li>
+          <Link href={'/posts'}>
+            <a>Blog</a>
           </Link>
         </li>
-      ))}
+        <li>Github</li>
+        <li>About</li>
       </ul>
+    </p>
+    <div class="space" />
+    <div class="contacts">
+      <>Contact</><br />
+        <a href="mailto:zohaibansari100@gmail.com">zohaibansari100@gmail.com</a>
+    </div>
+    </>
         )
 }
---
+++ pages/posts/index.js
@@ -0,0 +1,37 @@
+import { useEffect, useState } from 'react'
+import Head from 'next/head'
+import Post from '../../components/post'
+import Link from 'next/link'
+
+const client = require('contentful').createClient({
+  space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
+  accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
+})
+
+export async function getStaticProps(){
+  let data = await client.getEntries({
+    content_type: 'post'
+  })
+
+  return {
+    props: {
+      posts: data.items
+    },
+    revalidate: 1
+  }
+}
+
+export default function Home(posts){
+  console.log(posts.posts);
+  return (
+      <ul>
+      {posts.posts.map(article => (
+        <li key={article.sys.id}>
+          <Link href={'/posts/' + article.fields.slug}>
+          <a>{article.fields.title}</a>
+          </Link>
+        </li>
+      ))}
+      </ul>
+        )
+}
